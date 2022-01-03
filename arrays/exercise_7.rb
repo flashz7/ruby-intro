4d9843cd5ca7e819ed0f341c9bb07685adb42ec9
JB Colbert <jamescolbertv@gmail.com>
2022-01-02 22:07:50 -0800

    added in 'arrays' exercises

---
+++ arrays/exercise_7.rb
@@ -0,0 +1,2 @@
+afc_west_teams = ['chargers', 'broncos', 'raiders', 'chiefs']
+afc_west_teams.each_with_index { |team, idx| puts "#{idx + 1}. #{team}"}
---
+++ arrays/exercise_8.rb
@@ -0,0 +1,5 @@
+first_array = [5, 4, 3, 2, 1]
+
+second_array = first_array.map { |x| x + 2}
+
+p first_array, second_array
